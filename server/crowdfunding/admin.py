3eb4707a5f717c95179c6a1358162ac7bf51b274
Shrey Sumariya <shrey.sumariya@gmail.com>
2022-03-12 14:12:53 +0530

    CrowdFunding Models created

--- server/crowdfunding/admin.py
+++ server/crowdfunding/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Funding)
+admin.site.register(FundingReviews)
+admin.site.register(Donation)
--- server/crowdfunding/models.py
+++ server/crowdfunding/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+
+def get_Funding_Image(self, filename):
+    return 'images/{0}/{1}'.format(self.creator_id.id, filename)
+
+
+class Funding(models.Model):
+    creator_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    fund_cause = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=10)
+    total_fund = models.IntegerField()
+    amount_raise = models.IntegerField()
+    description = models.CharField(max_length=2000)
+    image = models.ImageField(upload_to=get_Funding_Image, default='no-image')
+
+
+class FundingReviews(models.Model):
+    funding_id = models.ForeignKey(Funding, on_delete=models.CASCADE)
+    email = models.EmailField()
+    name = models.CharField(max_length=200)
+    message = models.CharField(max_length=300)
+
+
+class Donation(models.Model):
+    donar_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    funding_id = models.ForeignKey(Funding, on_delete=models.CASCADE)
+    email = models.EmailField()
+    name = models.CharField()
+    amount_donated = models.IntegerField()
